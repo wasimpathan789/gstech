6e5e9265bbe0876778b760a842ec55daa89c8ef1
ishu <er.wasimpathan789@gmail.com>
2024-03-31 09:39:02 +0530

    favicon and text added and footer animation added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,19 @@ import { FaLinkedin, FaInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footerTop">
-        <div className="row">
-          <div className="footer-col">
-            <h4>company</h4>
-            <ul>
-              <li>
-                <a href="#">
+      <div data-aos="fade-right" data-aos-duration="1000" className="footerTop">
+        <div data-aos="fade-right" data-aos-duration="1000" className="row">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            className="footer-col"
+          >
+            <h4 data-aos="fade-right" data-aos-duration="1000">
+              company
+            </h4>
+            <ul data-aos="fade-right" data-aos-duration="1000">
+              <li data-aos="fade-right" data-aos-duration="1000">
+                <a data-aos="fade-right" data-aos-duration="1000" href="#">
                   KSA-based, GS Tech is a technology and technology firm
                   offering future-ready solutions to its clients across the
                   globe. Our experience is designing smart and scalable business
@@ -19,55 +25,87 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="footer-col">
-            <h4>Address</h4>
-            <ul>
-              <li>
-                <a href="#">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            className="footer-col"
+          >
+            <h4 data-aos="fade-right" data-aos-duration="1000">
+              Address
+            </h4>
+            <ul data-aos="fade-right" data-aos-duration="1000">
+              <li data-aos="fade-right" data-aos-duration="1000">
+                <a data-aos="fade-right" data-aos-duration="1000" href="#">
                   Abu Bakr Siddique, Narjis Building, Post Box - 12301, Riyadh -
                   11473, Saudi Arabia.
                 </a>
               </li>
             </ul>
           </div>
-          <div className="footer-col">
-            <h4>Email Address</h4>
-            <ul>
-              <li>
-                <a href="#">hello@gstech.com.sa</a>
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="footer-col"
+          >
+            <h4 data-aos="fade-left" data-aos-duration="1000">
+              Email Address
+            </h4>
+            <ul data-aos="fade-left" data-aos-duration="1000">
+              <li data-aos="fade-left" data-aos-duration="1000">
+                <a data-aos="fade-left" data-aos-duration="1000" href="#">
+                  hello@gstech.com.sa
+                </a>
               </li>
-              <li>
-                <a href="#">hello@gstech.com.sa</a>
+              <li data-aos="fade-left" data-aos-duration="1000">
+                <a data-aos="fade-left" data-aos-duration="1000" href="#">
+                  hello@gstech.com.sa
+                </a>
               </li>
             </ul>
           </div>
-		  <div className="footer-col">
-            <h4>Email Address</h4>
-            <ul>
-              <li>
-                <a href="#">hello@gstech.com.sa</a>
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="footer-col"
+          >
+            <h4 data-aos="fade-left" data-aos-duration="1000">
+              Email Address
+            </h4>
+            <ul data-aos="fade-left" data-aos-duration="1000">
+              <li data-aos="fade-left" data-aos-duration="1000">
+                <a data-aos="fade-left" data-aos-duration="1000" href="#">
+                  hello@gstech.com.sa
+                </a>
               </li>
-              <li>
-                <a href="#">hello@gstech.com.sa</a>
+              <li data-aos="fade-left" data-aos-duration="1000">
+                <a data-aos="fade-left" data-aos-duration="1000" href="#">
+                  hello@gstech.com.sa
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="footerBottom">
-        <h4>follow us</h4>
-        <div className="social-links">
-          <a href="#">
+      <div data-aos="fade-up" data-aos-duration="1000" className="footerBottom">
+        <h4 data-aos="fade-up" data-aos-duration="1000">
+          follow us
+        </h4>
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          className="social-links"
+        >
+          <a data-aos="fade-up" data-aos-duration="1000" href="#">
             <FaLinkedin style={{ fontSize: "40px" }} />
           </a>
-          <a href="#">
+          <a data-aos="fade-up" data-aos-duration="1000" href="#">
             <FaInstagram style={{ fontSize: "40px" }} />
           </a>
 
-          <a href="#">
+          <a data-aos="fade-up" data-aos-duration="1000" href="#">
             <FaLinkedin style={{ fontSize: "40px" }} />
           </a>
-          <a href="#">
+          <a data-aos="fade-up" data-aos-duration="1000" href="#">
             <FaInstagram style={{ fontSize: "40px" }} />
           </a>
         </div>
